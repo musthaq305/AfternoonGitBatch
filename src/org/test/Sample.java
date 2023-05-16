90ec3e6926d8343308b0d71ec4bf4118ab911c43
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-16 12:52:39 +0530

    added few lines of code

---
+++ src/org/test/Sample.java
@@ -0,0 +1,24 @@
+package org.test;
+
+public class Sample {
+
+public static void main(String[] args) {
+	
+	//While loop
+	
+		//Initialization
+		int i=7;
+		
+		//Condition
+		while (i<7) {
+			
+			//Logic
+			System.out.println(i);
+			
+			//Inc/Dec
+			i++;
+		}
+	
+	
+}
+}
