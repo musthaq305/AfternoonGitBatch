81bc8612690b1f59c16be22b68eabe344f135570
musthaq305 <musthaqmohamed305@gmail.com>
2023-05-17 12:48:11 +0530

    Anu added her code

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -2,10 +2,12 @@ package org.test;
 
 public class Sample {
 
+	private void m1() {
+    System.out.println("Anu Method");
+	}
 public static void main(String[] args) {
 	
-	System.out.println("Data");
-	System.out.println("sample");
+	System.out.println("Anu");
 	
 }
 }
